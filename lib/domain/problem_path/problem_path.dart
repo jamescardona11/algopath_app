49549c1e8a1b46908942a71426be85798ac3ecd8
jamescardona11 <65878592+jamescardona11@users.noreply.github.com>
2024-11-30 00:23:36 -0500

    ✨ feat: TAS-25 Problem Path Model

---
+++ lib/domain/problem_path/problem_path.dart
@@ -0,0 +1,33 @@
+import 'package:algopath_app/domain/types/json_type.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+import 'path_section.dart';
+
+part 'problem_path.freezed.dart';
+part 'problem_path.g.dart';
+
+const int defaultIconColor = 0xFF9E9E9E;
+
+@freezed
+class ProblemPath with _$ProblemPath {
+  const factory ProblemPath({
+    required int id,
+    required String name,
+    required String slug,
+    String? description,
+    int? iconId,
+    @Default(defaultIconColor) int iconColor,
+    @Default(true) bool isEnabled,
+    @Default(true) bool isEditable,
+    @Default([]) List<int> problemsIds,
+    @Default([]) List<PathSection> sections,
+    @Default('') String notes,
+    @Default(10) int order,
+  }) = _ProblemPath;
+
+  const ProblemPath._();
+
+  factory ProblemPath.fromJson(JsonType json) => _$ProblemPathFromJson(json);
+
+  String get dbId => id.toString();
+}
