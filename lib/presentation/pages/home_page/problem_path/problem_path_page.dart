86d6cdd795eca121165e22472de29126fae70c0e
jamescardona11 <65878592+jamescardona11@users.noreply.github.com>
2024-12-07 10:32:59 -0500

    ♻️ refactor: Change to sliver and pinned header

--- lib/presentation/pages/home_page/problem_path/problem_path_page.dart
+++ lib/presentation/pages/home_page/problem_path/problem_path_page.dart
@@ -19,57 +19,208 @@ class ProblemPathPage extends StatelessWidget {
       body: Container(
         width: 1200,
         padding: EdgeInsets.all(AppDimens.defaultPadding),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            ProblemPathHeader(),
-            const SizedBox(height: 40),
-            Expanded(
-              child: SizedBox(
-                // width: 1250,
-                child: ListView(
-                  children: [
-                    ProblemListItem(
-                      number: '416',
-                      title: 'Partition Equal Subset Sum',
-                      tags: ['Dynamic Programming', 'Medium'],
-                    ),
-                    ProblemListItem(
-                      number: '322',
-                      title: 'Coin Change',
-                      tags: ['Dynamic Programming', 'Medium'],
-                    ),
-                    ProblemListItem(
-                      number: '124',
-                      title: 'Binary Tree Maximum Path Sum',
-                      tags: ['Depth-first Search', 'Tree', 'Hard'],
-                    ),
-                    ProblemListItem(
-                      number: '494',
-                      title: 'Target Sum',
-                      tags: ['Dynamic Programming', 'Depth-first Search', 'Medium'],
-                    ),
-                    ProblemListItem(
-                      number: '1',
-                      title: 'Two Sum',
-                      tags: ['Easy'],
-                    ),
-                    ProblemListItem(
-                      number: '3',
-                      title: 'Longest Substring Without Repeating Characters',
-                      tags: ['Two Pointers', 'Medium'],
-                    ),
-                    ProblemListItem(
-                      number: '33',
-                      title: 'Search in Rotated Sorted Array',
-                      tags: ['Binary Search', 'Medium'],
-                    ),
-                  ],
+        child: NestedScrollView(
+          headerSliverBuilder: (context, innerBoxIsScrolled) {
+            return [
+              SliverOverlapAbsorber(
+                handle: NestedScrollView.sliverOverlapAbsorberHandleFor(context),
+                sliver: SliverPersistentHeader(
+                  pinned: true,
+                  delegate: SliverProblemPathHeader(),
                 ),
               ),
-            ),
-          ],
+            ];
+          },
+          body: Builder(
+            builder: (context) {
+              return CustomScrollView(
+                slivers: [
+                  SliverOverlapInjector(
+                    handle: NestedScrollView.sliverOverlapAbsorberHandleFor(context),
+                  ),
+                  SliverList(
+                    delegate: SliverChildListDelegate([
+                      ProblemListItem(
+                        number: '416',
+                        title: 'Partition Equal Subset Sum',
+                        tags: ['Dynamic Programming', 'Medium'],
+                      ),
+                      ProblemListItem(
+                        number: '322',
+                        title: 'Coin Change',
+                        tags: ['Dynamic Programming', 'Medium'],
+                      ),
+                      ProblemListItem(
+                        number: '124',
+                        title: 'Binary Tree Maximum Path Sum',
+                        tags: ['Depth-first Search', 'Tree', 'Hard'],
+                      ),
+                      ProblemListItem(
+                        number: '494',
+                        title: 'Target Sum',
+                        tags: ['Dynamic Programming', 'Depth-first Search', 'Medium'],
+                      ),
+                      ProblemListItem(
+                        number: '1',
+                        title: 'Two Sum',
+                        tags: ['Easy'],
+                      ),
+                      ProblemListItem(
+                        number: '3',
+                        title: 'Longest Substring Without Repeating Characters',
+                        tags: ['Two Pointers', 'Medium'],
+                      ),
+                      ProblemListItem(
+                        number: '33',
+                        title: 'Search in Rotated Sorted Array',
+                        tags: ['Binary Search', 'Medium'],
+                      ),
+                      ProblemListItem(
+                        number: '416',
+                        title: 'Partition Equal Subset Sum',
+                        tags: ['Dynamic Programming', 'Medium'],
+                      ),
+                      ProblemListItem(
+                        number: '322',
+                        title: 'Coin Change',
+                        tags: ['Dynamic Programming', 'Medium'],
+                      ),
+                      ProblemListItem(
+                        number: '124',
+                        title: 'Binary Tree Maximum Path Sum',
+                        tags: ['Depth-first Search', 'Tree', 'Hard'],
+                      ),
+                      ProblemListItem(
+                        number: '494',
+                        title: 'Target Sum',
+                        tags: ['Dynamic Programming', 'Depth-first Search', 'Medium'],
+                      ),
+                      ProblemListItem(
+                        number: '1',
+                        title: 'Two Sum',
+                        tags: ['Easy'],
+                      ),
+                      ProblemListItem(
+                        number: '3',
+                        title: 'Longest Substring Without Repeating Characters',
+                        tags: ['Two Pointers', 'Medium'],
+                      ),
+                      ProblemListItem(
+                        number: '33',
+                        title: 'Search in Rotated Sorted Array',
+                        tags: ['Binary Search', 'Medium'],
+                      ),
+                      ProblemListItem(
+                        number: '416',
+                        title: 'Partition Equal Subset Sum',
+                        tags: ['Dynamic Programming', 'Medium'],
+                      ),
+                      ProblemListItem(
+                        number: '322',
+                        title: 'Coin Change',
+                        tags: ['Dynamic Programming', 'Medium'],
+                      ),
+                      ProblemListItem(
+                        number: '124',
+                        title: 'Binary Tree Maximum Path Sum',
+                        tags: ['Depth-first Search', 'Tree', 'Hard'],
+                      ),
+                      ProblemListItem(
+                        number: '494',
+                        title: 'Target Sum',
+                        tags: ['Dynamic Programming', 'Depth-first Search', 'Medium'],
+                      ),
+                      ProblemListItem(
+                        number: '1',
+                        title: 'Two Sum',
+                        tags: ['Easy'],
+                      ),
+                      ProblemListItem(
+                        number: '3',
+                        title: 'Longest Substring Without Repeating Characters',
+                        tags: ['Two Pointers', 'Medium'],
+                      ),
+                      ProblemListItem(
+                        number: '33',
+                        title: 'Search in Rotated Sorted Array',
+                        tags: ['Binary Search', 'Medium'],
+                      ),
+                      ProblemListItem(
+                        number: '416',
+                        title: 'Partition Equal Subset Sum',
+                        tags: ['Dynamic Programming', 'Medium'],
+                      ),
+                      ProblemListItem(
+                        number: '322',
+                        title: 'Coin Change',
+                        tags: ['Dynamic Programming', 'Medium'],
+                      ),
+                      ProblemListItem(
+                        number: '124',
+                        title: 'Binary Tree Maximum Path Sum',
+                        tags: ['Depth-first Search', 'Tree', 'Hard'],
+                      ),
+                      ProblemListItem(
+                        number: '494',
+                        title: 'Target Sum',
+                        tags: ['Dynamic Programming', 'Depth-first Search', 'Medium'],
+                      ),
+                      ProblemListItem(
+                        number: '1',
+                        title: 'Two Sum',
+                        tags: ['Easy'],
+                      ),
+                      ProblemListItem(
+                        number: '3',
+                        title: 'Longest Substring Without Repeating Characters',
+                        tags: ['Two Pointers', 'Medium'],
+                      ),
+                      ProblemListItem(
+                        number: '33',
+                        title: 'Search in Rotated Sorted Array',
+                        tags: ['Binary Search', 'Medium'],
+                      ),
+                      ProblemListItem(
+                        number: '416',
+                        title: 'Partition Equal Subset Sum',
+                        tags: ['Dynamic Programming', 'Medium'],
+                      ),
+                      ProblemListItem(
+                        number: '322',
+                        title: 'Coin Change',
+                        tags: ['Dynamic Programming', 'Medium'],
+                      ),
+                      ProblemListItem(
+                        number: '124',
+                        title: 'Binary Tree Maximum Path Sum',
+                        tags: ['Depth-first Search', 'Tree', 'Hard'],
+                      ),
+                      ProblemListItem(
+                        number: '494',
+                        title: 'Target Sum',
+                        tags: ['Dynamic Programming', 'Depth-first Search', 'Medium'],
+                      ),
+                      ProblemListItem(
+                        number: '1',
+                        title: 'Two Sum',
+                        tags: ['Easy'],
+                      ),
+                      ProblemListItem(
+                        number: '3',
+                        title: 'Longest Substring Without Repeating Characters',
+                        tags: ['Two Pointers', 'Medium'],
+                      ),
+                      ProblemListItem(
+                        number: '33',
+                        title: 'Search in Rotated Sorted Array',
+                        tags: ['Binary Search', 'Medium'],
+                      ),
+                    ]),
+                  ),
+                ],
+              );
+            },
+          ),
         ),
       ),
     );
