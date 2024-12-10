a8302d384ada53c65cd5ab7ea4aa8498c60f7407
jamescardona11 <65878592+jamescardona11@users.noreply.github.com>
2024-12-10 17:29:49 -0500

    Update problem list items

--- lib/presentation/pages/home_page/problem_path/widgets/problem_list_items.dart
+++ lib/presentation/pages/home_page/problem_path/widgets/problem_list_items.dart
@@ -1,14 +1,18 @@
 import 'package:algopath_app/domain/problem/problem.dart';
+import 'package:algopath_app/domain/problem/topic_tag/topic_tag.dart';
 import 'package:algopath_app/presentation/widgets/problem_item.dart';
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 
 class ProblemListItems extends StatelessWidget {
   const ProblemListItems({
     super.key,
     required this.problems,
+    required this.topicTags,
   });
 
   final List<Problem> problems;
+  final Map<String, TopicTag> topicTags;
 
   @override
   Widget build(BuildContext context) {
@@ -16,11 +20,11 @@ class ProblemListItems extends StatelessWidget {
       delegate: SliverChildBuilderDelegate(
         (context, index) {
           final problem = problems[index];
+          final tags = problem.topicTags.map((tag) => topicTags[tag]).whereNotNull().toList();
 
           return ProblemItem(
-            number: problem.id.toString(),
-            title: problem.title,
-            tags: problem.topicTags,
+            problem: problem,
+            tags: tags,
           );
         },
         childCount: problems.length,
