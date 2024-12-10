142ebe77fc79e2163f418bd4b24bcfc6de042c55
jamescardona11 <65878592+jamescardona11@users.noreply.github.com>
2024-12-09 19:10:04 -0500

    Create problem data model

--- lib/presentation/pages/home_page/problem_path/provider/problem_data.dart
+++ lib/presentation/pages/home_page/problem_path/provider/problem_data.dart
@@ -1,21 +1,22 @@
 import 'package:algopath_app/domain/problem/problem.dart';
 
-sealed class ListElementData {
+abstract class ListElementData {
   bool get isProblemData => this is ProblemData;
   bool get isSectionData => this is SectionData;
 }
 
 class ProblemData extends ListElementData {
   final Problem problem;
+
   ProblemData(this.problem);
 }
 
-class SectionData extends ListElementData {
-  final String title;
+class SectionData<T> extends ListElementData {
+  final T section;
   final bool isExpanded;
 
   SectionData({
-    required this.title,
+    required this.section,
     this.isExpanded = false,
   });
 }
