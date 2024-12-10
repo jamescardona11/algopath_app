f66c9ea10345a851481b6c1b21c6d33568ee6b1d
jamescardona11 <65878592+jamescardona11@users.noreply.github.com>
2024-12-09 19:13:24 -0500

    Sort and group elements

--- lib/presentation/pages/home_page/problem_path/provider/problem_path_provider.dart
+++ lib/presentation/pages/home_page/problem_path/provider/problem_path_provider.dart
@@ -1,11 +1,17 @@
 import 'dart:async';
 
+import 'package:algopath_app/core/extensions/enum_extensions.dart';
 import 'package:algopath_app/data/database/collection_name.dart';
 import 'package:algopath_app/data/database/db_service.dart';
 import 'package:algopath_app/data/mapper_helpers/problem_mapper.dart';
 import 'package:algopath_app/domain/problem/problem.dart';
 import 'package:algopath_app/domain/problem_path/problem_path.dart';
+import 'package:algopath_app/presentation/pages/home_page/problem_path/provider/filters.dart';
+import 'package:algopath_app/utils/strings_extension.dart';
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
+
+import 'problem_data.dart';
 
 class ProblemPathProvider extends ChangeNotifier {
   final String _slug;
@@ -16,10 +22,23 @@ class ProblemPathProvider extends ChangeNotifier {
   }
 
   StreamSubscription? _problemsStream;
+  List<ListElementData> _listElementData = [];
   List<Problem> _problems = [];
   ProblemPath? _problemPath;
+  SortProblemsBy _sortProblemsBy = SortProblemsBy.none;
+  GroupProblemsBy _groupProblemsBy = GroupProblemsBy.none;
 
-  List<Problem> get problems => _problems;
+  List<ListElementData> get listElementData => _listElementData;
+
+  void changeGroupProblemsBy(GroupProblemsBy groupProblemsBy) {
+    _groupProblemsBy = groupProblemsBy;
+    notifyListeners();
+  }
+
+  void changeSortProblemsBy(SortProblemsBy sortProblemsBy) {
+    _sortProblemsBy = sortProblemsBy;
+    notifyListeners();
+  }
 
   Future<void> _initSubscription() async {
     _problemPath = await _db.run(CollectionName.problemPaths).read(id: _slug).then((json) {
@@ -42,9 +61,72 @@ class ProblemPathProvider extends ChangeNotifier {
         .map((jsonList) => ProblemMapper.fromJsonList(jsonList))
         .listen((problems) {
       _problems = problems;
+      _addListElementData();
 
       notifyListeners();
     });
+  }
+
+  void _addListElementData() {
+    _listElementData = _groupProblemsBy.when(
+      {
+        GroupProblemsBy.none: () => _groupByNone(),
+        GroupProblemsBy.byDifficulty: () => _groupByDifficulty(),
+        GroupProblemsBy.byTopic: () => _groupByTopic(),
+        GroupProblemsBy.bySection: () => _groupBySection(),
+      },
+    );
+  }
+
+  List<ListElementData> _groupByNone() {
+    return _sortElements(_problems.map((p) => ProblemData(p)).toList());
+  }
+
+  List<ListElementData> _groupByDifficulty() {
+    final List<ListElementData> result = [];
+
+    _problems.groupListsBy((p) => p.difficulty).forEach((key, value) {
+      result.add(SectionData<String>(section: key.capitalize()));
+      final elements = _sortElements(value.map((p) => ProblemData(p)).toList());
+      result.addAll(elements);
+    });
+
+    return result;
+  }
+
+  List<ListElementData> _groupByTopic() {
+    final List<ListElementData> result = [];
+
+    _problems.groupListsBy((p) => p.topicTags.firstOrNull ?? 'No topic').forEach((key, value) {
+      result.add(SectionData<String>(section: key.capitalize()));
+      final elements = _sortElements(value.map((p) => ProblemData(p)).toList());
+      result.addAll(elements);
+    });
+
+    return result;
+  }
+
+  List<ListElementData> _groupBySection() {
+    if (_problemPath!.sections.isEmpty) return _groupByNone();
+    final List<ListElementData> result = [];
+
+    for (var section in _problemPath!.sections) {
+      result.add(SectionData<String>(section: section.name.capitalize()));
+      final elements = _sortElements(_problems.where((p) => section.problemsIds.contains(p.id)).map((p) => ProblemData(p)).toList());
+      result.addAll(elements);
+    }
+
+    return result;
+  }
+
+  List<ProblemData> _sortElements(List<ProblemData> elements) {
+    return _sortProblemsBy.when(
+      {
+        SortProblemsBy.byDifficulty: () => elements.sorted((a, b) => a.problem.difficultyValue.compareTo(b.problem.difficultyValue)),
+        SortProblemsBy.byName: () => elements.sorted((a, b) => a.problem.title.compareTo(b.problem.title)),
+      },
+      orElse: () => elements,
+    );
   }
 
   @override
